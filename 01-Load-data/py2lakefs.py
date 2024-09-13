d2fbbfb3cfed15dd61e7ba5d34ac75e05e72a7fd
jklee <jklee@euclidsoft.co.kr>
2024-09-13 13:34:06 +0900

    modify

--- 01-Load-data/py2lakefs.py
+++ 01-Load-data/py2lakefs.py
@@ -29,23 +29,82 @@ class LakeFS:
         self.configuration.host = CFG.lakefs_url
 
 
-    ''' CREATE REPOSITORY '''
-    def create_repository(self):
-        # minio = connect_minio()
+    def create_lakefs_repository(self, 
+                                 minio_bucket_name, 
+                                 lakefs_repository_name, 
+                                 lakefs_branch_name
+                                 ):
+        minio = MinIO()
+        minio.create_bucket(minio_bucket_name)
+        
         client = LakeFSClient(self.configuration)
-
         try:
-        # Create repository
-            repository_creation = models.RepositoryCreation(name=self.lakefs_repository_name, 
-                                                            storage_namespace=f'{self.minio_service_name}://{self.lakefs_repository_name}', 
-                                                            default_branch=self.lakefs_branch_name)
-            
+            repository_creation = models.RepositoryCreation(
+                name=lakefs_repository_name,
+                description="",
+                storage_namespace=f"s3://{minio_bucket_name}",
+            )
             client.repositories.create_repository(repository_creation)
-            print("Repository created successfully.")
+            print("Repository created successfully")
+            
+            # Create a branch in the repository
+            branch_creation = models.BranchCreation(
+                name=lakefs_branch_name,
+                source="main"  # 기본 소스 브랜치를 'main'으로 설정
+            )
+            client.branches.create_branch(lakefs_repository_name, branch_creation)
+            print("Branch created successfully")
+            
+        except Exception as e:
+            print("Error creating repository or branch: ", str(e))
+            
 
+
+    ''' CREATE REPOSITORY '''
+    # def create_repository(self, lakefs_repository_name, lakefs_branch_name):
+    #     # minio = connect_minio()
+    #     client = LakeFSClient(self.configuration)
+
+    #     try:
+    #         repository_creation = models.RepositoryCreation(name=lakefs_repository_name, 
+    #                                                         # storage_namespace=f's3://{lakefs_repository_name}',
+    #                                                         storage_namespace=f's3://test',
+    #                                                         default_branch=lakefs_branch_name)
+            
+    #         client.repositories.create_repository(repository_creation)
+    #         print("Repository created successfully.")
+
+    #     except lakefs_client.ApiException as e:
+    #         print("Exception when calling RepositoriesApi -> create_repository: %s\n" % e)
+
+    def delete_repository(self, lakefs_repository_name):
+        client = LakeFSClient(self.configuration)
+        try:
+            client.repositories.delete_repository(lakefs_repository_name)
+            print("Repository deleted successfully.")
+            # minio.delete_bucket(self.lakefs_repository_name)
+            # minio.remove_all_objects(self.lakefs_repository_name)
+        except:
+            print("Error in deleting repository.")
+
+
+    def create_lakefs_branch(self, 
+                                 lakefs_repository_name, 
+                                 lakefs_branch_name
+                                 ):
+        client = LakeFSClient(self.configuration)
+        
+        try:
+            # Create a branch in the repository
+            branch_creation = models.BranchCreation(
+                name=lakefs_branch_name,
+                source="main"  # 기본 소스 브랜치를 'main'으로 설정
+            )
+            client.branches.create_branch(lakefs_repository_name, branch_creation)
+            print("Branch created successfully")
+            
         except lakefs_client.ApiException as e:
-            print("Exception when calling RepositoriesApi -> create_repository: %s\n" % e)
-
+            print("Exception when calling BranchesApi -> create_branch: %s\n" % e)
 
     ''' UPLOAD FILE '''
     def upload_file(self, file_path):
@@ -107,9 +166,9 @@ if __name__ == '__main__':
     lakefs = LakeFS()
     minio = MinIO()
     
-    minio.create_bucket()
-    lakefs.create_repository()
-    
+    # minio.create_bucket()
+    lakefs.create_lakefs_repository('abcdefgh1234', 'addgeneratorbucket1234', '123')
+    lakefs.create_lakefs_branch('addgeneratorbucket1234', 'test123')
     lakefs.upload_file(CFG.object_path)
     
     file_content = lakefs.get_lakefs_file_content('data')
--- 01-Load-data/py2minio.py
+++ 01-Load-data/py2minio.py
@@ -1,5 +1,7 @@
-import os
+import io
+import pandas as pd
 import base64
+import botocore
 import boto3
 
 # configuration python file
@@ -17,14 +19,13 @@ class MinIO:
             aws_access_key_id=CFG.minio_access_key, 
             aws_secret_access_key=CFG.minio_secret_key
         )
-        
-        
+
+
     ''' CREATE BUCKET '''
-    def create_bucket(self):
+    def create_bucket(self, minio_bucket_name):
         try:
-            self.minio_client.create_bucket(Bucket=self.minio_bucket_name)
-            print(f'bucket name: {self.minio_bucket_name}')
-            
+            self.minio_client.create_bucket(Bucket=minio_bucket_name)
+            print(f'bucket name: {minio_bucket_name}')
         except:
             print('Already own it')
             pass
@@ -47,8 +48,8 @@ class MinIO:
         print("Objects in bucket:")
         for obj in response.get('Contents', []):
             print(obj['Key'])
-    
-    
+
+
     ''' UPLOAD OBJECT2 '''
     def upload_fileobj(self):
         try:
@@ -67,31 +68,52 @@ class MinIO:
         print("Objects in bucket:")
         for obj in response.get('Contents', []):
             print(obj['Key'])
-
     
     
     ''' DOWNLOAD OBJECT '''
     def download_object(self):
         self.minio_client.download_file(CFG.minio_bucket_name,
                                         CFG.minio_object_name, 
-                                        CFG.data_path)
+                                        'tmp')
 
 
     ''' GET OBJECT '''
     def get_content_in_object(self):
+        # response = self.minio_client.get_object(Bucket=CFG.minio_bucket_name, 
+        #                                         Key=CFG.minio_object_name)
+        
         response = self.minio_client.get_object(Bucket=CFG.minio_bucket_name, 
-                                                Key=CFG.minio_object_name)
+                                                Key='add-augmentation-bucket/202408/')
         
         data = response['Body'].read().decode('utf-8')
 
         return data
 
+    def get_object_list(self):
+        response = self.minio_client.list_objects(Bucket='add-augmentation-bucket',
+                                                  Prefix='add-augmentation-bucket/202408')
+
+        # 객체 목록 출력
+        print("Objects in bucket:")
+        for idx, obj in enumerate(response.get('Contents', [])):
+            print(obj['Key'])
+            response = self.minio_client.get_object(Bucket='add-augmentation-bucket', 
+                                                    Key=obj['Key'])
+
+            data = response['Body'].read().decode('utf-8')
+            output = io.StringIO(data)
+            minio_data = pd.read_xml(output, parser='etree')
+            minio_data.to_xml(f'./{idx}.xml', parser='etree', index=False, root_name='Shots', row_name='Shot')
+        
 
 
 if __name__ == '__main__':
     minio = MinIO()
     
-    minio.create_bucket()
-    minio.upload_object()
+    # minio.create_bucket()
+    # minio.upload_object()
     # minio.upload_fileobj()
-    print('')
+    # minio.download_object()
+    # minio.get_object_list()
+    minio.get_content_in_object()
+    
